77a364c5510bb77cd077dd8aebeb1362faa87852
edson.gutierrez1@unmsm.edu.pe <edsongutierrez789123@gmail.com>
2024-05-27 19:11:15 -0500

    Avance rewards

--- app/src/main/java/com/example/naturetrustbank/ui/rewards/RewardsViewModel.kt
+++ app/src/main/java/com/example/naturetrustbank/ui/rewards/RewardsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class RewardsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is reward Fragment"
     }
     val text: LiveData<String> = _text
 }
